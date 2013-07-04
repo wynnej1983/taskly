a72a8345e4b8d7f2fe3b9cd794a3642048212595
Jonathan Wynne <wynnej1983@gmail.com>
2013-07-05 01:05:27 +0900

    added REST endpoint to return lastaction so could show user flash msg and option to undo

--- routes/tasks.js
+++ routes/tasks.js
@@ -7,6 +7,15 @@ var _ = require('lodash')
 
 exports.mapRoutes = function (app) {
 
+  app.get('/api/v1/tasks/getLastAction:format(.json|.xml)?', setContentTypeFromResourceExtname, function (req, res) {
+      if (res.format === 'xml') {
+        var lastActionXml = new Js2Xml('lastAction', req.session.lastAction).toString();
+        res.send(lastActionXml);
+      } else {
+        res.send(req.session.lastAction);
+      }
+  });
+
   app.get('/api/v1/tasks:format(.json|.xml)?', setContentTypeFromResourceExtname, function (req, res) {
     Task.find(function (err, tasks) {
       if (err) {
@@ -29,8 +38,11 @@ exports.mapRoutes = function (app) {
         console.log(err);
         return res.send(404, { error: 'could not find task with id ' + id });
       }
+      task = task.toObject();
+      task.lastAction = req.session.lastAction;
+      console.log(task);
       if (res.format === 'xml') {
-        var taskXml = new Js2Xml('task', {_id: task._id.toString(), user_id: task.user_id, name: task.name, done: task.done, __v: task.__v}).toString();
+        var taskXml = new Js2Xml('task', task).toString();
         res.send(taskXml);
       } else {
         res.send(task);
@@ -48,8 +60,11 @@ exports.mapRoutes = function (app) {
         console.log(err);
         return res.send(400, { error: 'could not create task due to bad request' });
       }
+      task = task.toObject();
+      req.session.lastAction = {'POST': {_id: task._id, user_id: task.user_id, name: task.name, done: task.done}};
+      task.lastAction = req.session.lastAction;
       if (res.format === 'xml') {
-        var taskXml = new Js2Xml('task', {_id: task._id.toString(), user_id: task.user_id, name: task.name, done: task.done, __v: task.__v}).toString();
+        var taskXml = new Js2Xml('task', task).toString();
         res.send(taskXml);
       } else {
         res.location('/api/v1/tasks/' + task._id)
@@ -65,8 +80,11 @@ exports.mapRoutes = function (app) {
         console.log(err);
         return res.send(400, { error: 'could not update task due to bad request' });
       }
+      task = task.toObject();
+      req.session.lastAction = {'PUT': {_id: task._id, user_id: task.user_id, name: task.name, done: task.done}};
+      task.lastAction = req.session.lastAction;
       if (res.format === 'xml') {
-        var taskXml = new Js2Xml('task', {_id: task._id.toString(), user_id: task.user_id, name: task.name, done: task.done, __v: task.__v}).toString();
+        var taskXml = new Js2Xml('task', task).toString();
         res.send(taskXml);
       } else {
         res.send(task);
@@ -76,13 +94,15 @@ exports.mapRoutes = function (app) {
 
   app.del('/api/v1/tasks/:id:format(.json|.xml)?', setContentTypeFromResourceExtname, function (req, res) {
     var id = req.params.id;
-    Task.findByIdAndRemove(id, function (err) {
+    Task.findByIdAndRemove(id, function (err, task) {
       if (err) {
         console.log(err);
         return res.send(400, { error: 'could not delete task with id ' + id + ' due to bad request' });
       }
+      task = task.toObject();
+      req.session.lastAction = {'DELETE': task};
       if (res.format === 'xml') {
-        var taskXml = new Js2Xml('task', {_id: task._id.toString(), user_id: task.user_id, name: task.name, done: task.done, __v: task.__v}).toString();
+        var taskXml = new Js2Xml('task', {_id: task._id, user_id: task.user_id, name: task.name, done: task.done}).toString();
         res.send(taskXml);
       } else {
         res.send(204, { success: 'task was deleted' });
