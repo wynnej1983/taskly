42743b329b8767d9e6daf8b5a4d328a63ac5f246
Jonathan Wynne <wynnej1983@gmail.com>
2013-11-09 16:22:37 +0900

    swapped backend persistance with Firebase service

--- app/js/services.js
+++ app/js/services.js
@@ -1,33 +1,27 @@
 'use strict';
 
 angular
-  .module('services', ['ngResource'])
-	.factory('taskService', function ($resource) {
-		var Task = $resource(
-      '/api/v1/tasks/:id'
-    , {id: '@_id'}
-    , { 'update': { method: 'PUT' } });
+  .module('services', ['firebase'])
+	.factory('taskService', function (angularFireCollection) {
+    var url = 'https://taskly.firebaseio.com/tasks';
+    var ref = new Firebase(url);
+    var tasks = null;
 
     return {
       getTasks: function (limit, offset) {
-        return Task.query({limit: limit, offset: offset});
+        tasks = angularFireCollection(ref.limit(limit));
+        return tasks;
       },
       saveTask: function (task, cb) {
-        var update = !!task._id;
-        task = new Task(task);
-
-        return update ?
-         task.$update() :
-         task.$save(function (i) {
-          if (cb) {
-            cb(i);
-          }
-        });
+        //if (tasks.indexOf(task) > -1) {
+          //tasks.update(task);
+        //} else {
+          tasks.add(task);
+        //}
+        cb(task);
       },
       deleteTask: function (task) {
-        task = new Task(task);
-
-        return task.$delete();
+        tasks.remove(task);
       }
     };
 	});
